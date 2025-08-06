30da3a803f1ea8bd2bafbdb1b995ad5efa8bd609
onurerguden <119923881+onurerguden@users.noreply.github.com>
2025-08-06 18:28:16 +0300

    Backend Added

---
+++ taskfoo-backend/src/main/java/com/taskfoo/taskfoo_backend/TaskfooBackendApplication.java
@@ -0,0 +1,13 @@
+package com.taskfoo.taskfoo_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskfooBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskfooBackendApplication.class, args);
+	}
+
+}
---
+++ taskfoo-backend/src/test/java/com/taskfoo/taskfoo_backend/TaskfooBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taskfoo.taskfoo_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskfooBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
