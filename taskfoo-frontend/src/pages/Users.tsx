4a53563d60c1e1ba0737368d4cb586aa3a3303f8
onurerguden <119923881+onurerguden@users.noreply.github.com>
2025-08-19 12:17:57 +0300

    Updates

--- taskfoo-frontend/src/pages/Users.tsx
+++ taskfoo-frontend/src/pages/Users.tsx
@@ -2,7 +2,7 @@
 import { useMemo } from "react";
 import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
 import { Table, Typography, Alert, Avatar, Space, Button, Popconfirm, App, Tag } from "antd";
-import { UserOutlined, DeleteOutlined, EyeOutlined } from "@ant-design/icons";
+import { DeleteOutlined, EyeOutlined } from "@ant-design/icons";
 import { useNavigate } from "react-router-dom";
 import api from "../api/client";
 import { type TaskListItemResponse, type UserBrief } from "../api/tasks";
